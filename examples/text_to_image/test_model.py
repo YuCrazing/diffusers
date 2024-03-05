fb2dca5d08995df00fefe0ff4b3013802e410a71
yucrazing <723284893@qq.com>
2024-03-05 23:36:10 +0800

    Update code

--- examples/text_to_image/test_model.py
+++ examples/text_to_image/test_model.py
@@ -3,7 +3,6 @@ from diffusers.utils import make_image_grid
 
 import torch
 
-# model_path = "sd-model-finetuned-1500-clean"
 model_path = "sd-model-finetuned-15000"
 pipeline = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float32, use_safetensors=True).to("cuda")
 
@@ -17,11 +16,16 @@ if ckp != "":
 
 pipeline.to("cuda")
 
+prompt = "yoda"
 generator = [torch.Generator(device="cuda").manual_seed(i) for i in range(4)]
-images = pipeline("yoda", generator=generator, num_images_per_prompt=4).images
-
+images = pipeline(prompt, generator=generator, num_images_per_prompt=4).images
 
 # make_image_grid(images, rows=2, cols=2).show()
+
+import os
+output_dir = "output"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 for i in range(len(images)):
     image = images[i]
-    image.save(f"image-{i}-{ckp}.png")
+    image.save(f"{output_dir}/image-{i}-{ckp}-{prompt}.png")
